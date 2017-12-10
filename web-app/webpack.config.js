cc1ba18681a5b06dd8253d12b3c0b55fc57e6605
Wendy Hsu <wndyhsu@gmail.com>
2017-12-10 12:56:40 -0800

    poor ei

---
+++ web-app/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+
+    ]
+  }
+}
